6c8acd924a82312ea62954ef313c3627cc6a762c
Louise Flinta <louise.flinta@tietoevry.com>
2021-12-08 13:12:40 +0100

    Added input files as well as starting day1part1

--- src/year2021/day1/day1part1.clj
+++ src/year2021/day1/day1part1.clj
@@ -1,4 +1,27 @@
-(ns year2021.day1.day1part1)
-(defn hello-world []
-  (println "Hello World"))
-(hello-world)
+(ns year2021.day1.day1part1
+  (:require [clojure.string :as str]))
+
+(def test-input "src/year2021/day1/test-input.txt")
+(def input "src/year2021/day1/input.txt")
+
+(defn get-lines [file]
+  (str/split-lines (slurp file)))
+
+(def my-list (get-lines test-input))
+(def num-list (map read-string my-list))
+
+
+
+(println my-list)
+(println (type my-list))
+
+(println num-list)
+
+(loop [[head & tail] num-list
+       acc 0]
+  (println head)
+  (if (empty? tail)
+    (println acc)
+    (recur tail (inc acc))
+    )
+  )
