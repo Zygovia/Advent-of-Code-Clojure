2ad0b4bad55f081b94e2df5bb86dce2f9b5575bc
Louise Flinta <louise.flinta@tietoevry.com>
2022-10-27 14:35:55 +0200

    Kommentarer osv

--- src/year2021/basics/basics.clj
+++ src/year2021/basics/basics.clj
@@ -1,22 +1,48 @@
 (if (= (+ 2 2) 4)
   (println "Maths works!")                                  ; this gets evaluated if 2 + 2 = 4
-  (println "UH OH"))                                        ; this gets evaluated if 2 + 2 != 4
+
+  (println "UH OH")
+
+  )                                        ; this gets evaluated if 2 + 2 != 4
 ; => Maths works!
 ;    nil
 
 (if (= (+ 2 2) 4)
   (do                                                       ; all of this gets evaluated if 2 + 2 = 4
     (println "Maths works!")
-    (println "Maths still works!"))
-  (println "UH OH"))
+    (println "Maths still works!")
+    4)
+  (do
+    (println "UH OH")
+    "en sträng")
+  )
 ; => Maths works!
 ;    Maths still works!
 ;    nil
 
 (println (cond (and true false)
                "Nope"
-               (and true true)
+
+               (and true true false)
                "Yes"
-               (or true false)
+
+               (or false false)
                "Also yes"
+
+               true
+               123
                ))
+
+(println "yay")
+(def my-var 6.5)
+(println my-var)
+(+ 1 2)
+(+ my-var 4)
+
+(and 1 2 3)
+
+(def testv 76)
+(or nil 123)
+(or false nil false)
+
+(and  1 2 3)
--- src/year2021/day1/day1part1.clj
+++ src/year2021/day1/day1part1.clj
@@ -1,16 +1,21 @@
 (ns year2021.day1.day1part1
   (:require [clojure.string :as str]))
 
+;; Definierar två med paths till filerna
 (def test-input "src/year2021/day1/test-input.txt")
 (def input "src/year2021/day1/input.txt")
 
+;; Definierar funktion som heter get-lines,
+;; som tar emot ett arg file,
+;; och som anropar str/split-lines med resultatet
+;; av slurp file
 (defn get-lines [file]
   (str/split-lines (slurp file)))
 
+;; Skapar variabel my-list med test-input
+;; Skapar variabel num-list, som mappar
 (def my-list (get-lines test-input))
 (def num-list (map read-string my-list))
-
-
 
 (println my-list)
 (println (type my-list))
